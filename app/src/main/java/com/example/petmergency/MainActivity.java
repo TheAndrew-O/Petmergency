d58a0dc1c461135dd4c827dfd566387e0f7f86f2
Andrew Owens <owens518@umn.edu>
2022-09-15 21:33:56 -0500

    Pet App Template

---
+++ app/src/main/java/com/example/petmergency/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.petmergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button choke_but;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_main);
+
+        choke_but = findViewById(R.id.button_choke);
+        choke_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openChokeActivity();
+            }
+        });
+    }
+
+    public void openChokeActivity(){
+       Intent intent = new Intent(this, choking.class);
+       startActivity(intent);
+    }
+}
