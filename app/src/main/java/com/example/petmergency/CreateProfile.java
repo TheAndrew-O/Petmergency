a31965fe1759ed8a1580cff2e7ba1bd6add96680
Andrew Owens <owens518@umn.edu>
2022-11-08 16:29:23 -0600

    Added profile creation.

---
+++ app/src/main/java/com/example/petmergency/CreateProfile.java
@@ -0,0 +1,87 @@
+package com.example.petmergency;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class CreateProfile extends AppCompatActivity {
+
+    private final int GALLERY_REQUEST_CODE = 1000;
+    ImageView prof_img;
+    private Button create_prof_but;
+    private AnimationDrawable anim_background;
+//    RelativeLayout relativeLayout;
+    LinearLayout linearLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_create_profile);
+
+        linearLayout = (LinearLayout)findViewById(R.id.create_profile_card);
+        anim_background = (AnimationDrawable)linearLayout.getBackground();
+        anim_background.setEnterFadeDuration(5000);
+        anim_background.setExitFadeDuration(2000);
+
+        prof_img = findViewById(R.id.profile_pic);
+        Button select_pic = findViewById(R.id.select_profile_pic);
+        select_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_PICK);
+                gallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery,GALLERY_REQUEST_CODE);
+            }
+        });
+
+        create_prof_but = findViewById(R.id.submit_profile);
+        create_prof_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPetProfile();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQUEST_CODE){
+                prof_img.setImageURI(data.getData());
+            }
+        }
+    }
+
+    public void openPetProfile(){
+        Intent intent = new Intent(this, Dog3.class);
+        startActivity(intent);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        if(anim_background != null && !anim_background.isRunning()){
+            anim_background.start();
+        }
+    }
+    protected void onPause(){
+        super.onPause();
+        if(anim_background != null && !anim_background.isRunning()){
+            anim_background.start();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/petmergency/Dog3.java
@@ -0,0 +1,120 @@
+package com.example.petmergency;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class Dog3 extends AppCompatActivity {
+    private final int GALLERY_REQUEST_CODE = 1000;
+    ImageView prof_img;
+    private Button open_calendar;
+    private Button open_med;
+    private Button open_notes;
+    private ImageButton swap_but;
+    private ImageButton emergency_but;
+
+    String calendar = "com.google.android.calendar";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_dog3);
+
+        prof_img = findViewById(R.id.imgGallery);
+        swap_but = findViewById(R.id.switch_profile2);
+        swap_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSwapProfileActivity();
+            }
+        });
+
+        emergency_but = findViewById(R.id.home_screen);
+        swap_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+
+        open_calendar = findViewById(R.id.button_appoitments);
+        open_calendar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2022, 9, 23, 10, 30);
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2022, 9, 23, 11, 30);
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+                calendarIntent.putExtra(CalendarContract.Events.TITLE, "Pet's Appoitment");
+                calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Location");
+                startActivity(calendarIntent);
+            }
+        });
+
+        open_med = findViewById(R.id.button_medications);
+        open_med.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openMedActivity();
+            }
+        });
+
+        open_notes = findViewById(R.id.button_notes);
+        open_notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNotesActivity();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+            if(requestCode==GALLERY_REQUEST_CODE){
+                prof_img.setImageURI(data.getData());
+            }
+        }
+    }
+    public void openMedActivity(){
+        Intent intent = new Intent(this, medication.class);
+        startActivity(intent);
+    }
+
+    public void openNotesActivity(){
+        Intent intent = new Intent(this, Notes.class);
+        startActivity(intent);
+    }
+
+    public void openSwapProfileActivity(){
+        Intent intent = new Intent(this, ProfileSelector.class);
+        startActivity(intent);
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
