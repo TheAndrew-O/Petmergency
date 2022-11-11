d088c08dee1bdca2a82b64385ad61892f8a9775c
Adrian Bui <adrian.t.bui@gmail.com>
2022-11-11 11:25:38 -0600

    Re-implemented Note screen. Cannot save Notes for now. need to connect to db

--- app/src/main/java/com/example/petmergency/Notes.java
+++ app/src/main/java/com/example/petmergency/Notes.java
@@ -1,12 +1,20 @@
 package com.example.petmergency;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Notes extends AppCompatActivity {
+    private FloatingActionButton addNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,52 @@ public class Notes extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
-        setContentView(R.layout.activity_notes);
+        setContentView(R.layout.activity_notes_main);
+
+        addNoteButton = findViewById(R.id.addNoteBtn);
+        addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNote();
+            }
+        });
     }
+
+    private void addNote() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.activity_notes_input, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+
+        final EditText note = myView.findViewById(R.id.note);
+        final EditText description = myView.findViewById(R.id.description);
+        Button save = myView.findViewById(R.id.saveNoteBtn);
+        Button cancel = myView.findViewById(R.id.cancelNoteBtn);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mNote = note.getText().toString().trim();
+                String mDescription = description.getText().toString().trim();
+                //TODO: implement
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+
+
 }
