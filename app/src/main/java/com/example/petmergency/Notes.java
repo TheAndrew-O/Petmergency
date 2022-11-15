733de395ae87a768de8534dad13dcb1518db2228
Adrian Bui <adrian.t.bui@gmail.com>
2022-11-14 23:51:56 -0600

    notes_done

--- app/src/main/java/com/example/petmergency/Notes.java
+++ app/src/main/java/com/example/petmergency/Notes.java
@@ -3,18 +3,24 @@ package com.example.petmergency;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -27,6 +33,7 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class Notes extends AppCompatActivity {
+    private RecyclerView recyclerView;
     private DatabaseReference reference;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -130,6 +137,135 @@ public class Notes extends AppCompatActivity {
         dialog.show();
     }
 
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        View mView;
 
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
 
+        public void setNote(String note) {
+            TextView noteTectView = mView.findViewById(R.id.noteTv);
+            noteTectView.setText(note);
+        }
+
+        public void setDesc(String desc) {
+            TextView descTectView = mView.findViewById(R.id.descriptionTv);
+            descTectView.setText(desc);
+        }
+
+        public void setDate(String date) {
+            TextView dateTextView = mView.findViewById(R.id.dateTv);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Notes_Model> options = new FirebaseRecyclerOptions.Builder<Notes_Model>()
+                .setQuery(reference, Notes_Model.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Notes_Model, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Notes_Model, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull final Notes_Model model) {
+                holder.setDate(model.getDate());
+                holder.setNote(model.getNote());
+                holder.setDesc(model.getDescription());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        key = getRef(position).getKey();
+                        note = model.getNote();
+                        description = model.getDescription();
+
+                        updateNote();
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_notes_retrieve, parent, false);
+                return new MyViewHolder(view);
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    private void updateNote() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.activity_notes_update, null);
+        myDialog.setView(view);
+
+        final AlertDialog dialog = myDialog.create();
+
+        final EditText mNote = view.findViewById(R.id.mEditTextNote);
+        final EditText mDescription = view.findViewById(R.id.mEditTextDescription);
+
+        mNote.setText(note);
+        mNote.setSelection(note.length());
+
+        mDescription.setText(description);
+        mDescription.setSelection(description.length());
+
+        Button delButton = view.findViewById(R.id.btnDelete);
+        Button updateButton = view.findViewById(R.id.btnUpdate);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                note = mNote.getText().toString().trim();
+                description = mDescription.getText().toString().trim();
+
+                String date = DateFormat.getDateInstance().format(new Date());
+
+                Notes_Model model = new Notes_Model(note, description, key, date);
+
+                reference.child(key).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(Notes.this, "Data has been updated successfully", Toast.LENGTH_SHORT).show();
+                        }else {
+                            String err = task.getException().toString();
+                            Toast.makeText(Notes.this, "update failed "+err, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+                dialog.dismiss();
+
+            }
+        });
+
+        delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.child(key).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Notes.this, "Note deleted successfully", Toast.LENGTH_SHORT).show();
+                        }else {
+                            String err = task.getException().toString();
+                            Toast.makeText(Notes.this, "Failed to delete note "+ err, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
 }
